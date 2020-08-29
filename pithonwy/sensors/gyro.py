9cdae52e5403f3c3ded2857e5717b5a448eeac22
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2020-08-29 11:30:58 +0100

    Add calibration to Gyro class

--- pithonwy/sensors/gyro.py
+++ pithonwy/sensors/gyro.py
@@ -2,6 +2,7 @@
 import board
 import busio
 import adafruit_mpu6050
+from time import sleep
 
 class Gyro:
     """ Gyro class for both rotational and linear acceleration measurements """
@@ -9,6 +10,18 @@ class Gyro:
     def __init__(self):
         i2c = busio.I2C(board.SCL, board.SDA)
         self.mpu = adafruit_mpu6050.MPU6050(i2c)
+    
+    def calibrate(self):
+        """ Calibrates the readings when the gyro is stationary, so it can be properly 'zeroed' """
+        print("Calibrating Gyro. Please ensure the device is stationary, and the axes are aligned X forward, Y left, Z up")
+        print("Calbration starting in 3... ", end="")
+        sleep(1)
+        print("2... ", end="")
+        sleep(1)
+        print("1... ", end="")
+        sleep(1)
+        print("Calibration starting. Please keep the accelerometer completely stationary.")
+        #TODO: Write the jazz here to zero the readings
 
     def get_acceleration(self):
         return self.mpu.acceleration
