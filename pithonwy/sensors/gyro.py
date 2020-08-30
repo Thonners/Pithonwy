a391b1217f3dbf0484642793b9b9fa25bbc369a3
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2020-08-30 14:28:45 +0100

    Add option of normalisation of the gyro signals

--- pithonwy/sensors/gyro.py
+++ pithonwy/sensors/gyro.py
@@ -7,9 +7,20 @@ from time import sleep
 class Gyro:
     """ Gyro class for both rotational and linear acceleration measurements """
 
-    def __init__(self):
+    def __init__(self, normalise_rates=False, gyro_normalisation_values=[1,1,1], acceleration_normalisation_values=[1,1,1]):
+        """
+            Constructor for a Gyro object
+
+            Parameters:
+                normalise_rates: bool                       Whether to return the values for gyro or acceleration as their raw values (if false, default) or as a percentage of the normalisation values
+                gyro_normalisation_values: array            The raw value at which the rate = 1 if normalisation on
+                acceleration_normalisation_values: array    The raw value at which the rate = 1 if normalisation on
+        """
         i2c = busio.I2C(board.SCL, board.SDA)
         self.mpu = adafruit_mpu6050.MPU6050(i2c)
+        self.normalise_rates = normalise_rates
+        self.gyro_normalisation_values = gyro_normalisation_values
+        self.acceleration_normalisation_values = acceleration_normalisation_values
     
     def calibrate(self):
         """ Calibrates the readings when the gyro is stationary, so it can be properly 'zeroed' """
@@ -27,7 +38,12 @@ class Gyro:
         return self.mpu.acceleration
 
     def get_gyro(self):
-        return self.mpu.gyro
+        gyro = self.mpu.gyro
+        if self.normalise_rates:
+            normalised_gyro = [gyro[i] / self.gyro_normalisation_values[i] for i in range(3) ]
+            return normaalised_gyro
+        # Only get here if not normalising, so return the raw values
+        return gyro
 
     def get_temperature(self):
         return self.mpu.temperature
