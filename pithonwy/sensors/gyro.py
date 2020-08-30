8f7f471c7fc39f732dfaf1b8c4d55fe6da9e1e1e
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2020-08-30 18:36:44 +0100

    Update Gyro normalisation
    - Remove the boolean as it's unneeded
        - The default normalisation values of [1,1,1] do the same as setting normalisation to False

--- pithonwy/sensors/gyro.py
+++ pithonwy/sensors/gyro.py
@@ -12,9 +12,8 @@ class Gyro:
             Constructor for a Gyro object
 
             Parameters:
-                normalise_rates: bool                       Whether to return the values for gyro or acceleration as their raw values (if false, default) or as a percentage of the normalisation values
-                gyro_normalisation_values: array            The raw value at which the rate = 1 if normalisation on
-                acceleration_normalisation_values: array    The raw value at which the rate = 1 if normalisation on
+                gyro_normalisation_values: array            The raw value at which the normalised rate = 1. Default=[1,1,1] implies raw values are returned
+                acceleration_normalisation_values: array    The raw value at which the normalised rate = 1. Default=[1,1,1] implies raw values are returned
         """
         i2c = busio.I2C(board.SCL, board.SDA)
         self.mpu = adafruit_mpu6050.MPU6050(i2c)
@@ -39,11 +38,8 @@ class Gyro:
 
     def get_gyro(self):
         gyro = self.mpu.gyro
-        if self.normalise_rates:
-            normalised_gyro = [gyro[i] / self.gyro_normalisation_values[i] for i in range(3) ]
-            return normaalised_gyro
-        # Only get here if not normalising, so return the raw values
-        return gyro
+        normalised_gyro = [gyro[i] / self.gyro_normalisation_values[i] for i in range(3) ]
+        return normalised_gyro
 
     def get_temperature(self):
         return self.mpu.temperature
